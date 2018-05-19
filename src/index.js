7500fbd417152565a1fc867f1a000a18266b3c73
王志祥 <wzx361@126.com>
2018-05-19 17:51:44 +0800

    first-commit-counter_app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import ClickCounter from './ClickCounter'
+import ControlPanel from './ControlPanel'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<ControlPanel />, document.getElementById('root'));
+registerServiceWorker();
