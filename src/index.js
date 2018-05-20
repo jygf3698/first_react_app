7f8a1f84bbedd79f0c632c489f364e3a6e110a18
jygf3698 <jygf3698@126.com>
2018-05-20 22:01:55 +0800

    react-flux-app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 // import ClickCounter from './ClickCounter'
-import ControlPanel from './ControlPanel'
+import ControlPanel from './views/ControlPanel'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<ControlPanel />, document.getElementById('root'));
